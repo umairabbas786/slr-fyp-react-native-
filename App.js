410dd72d8a812b99be79111e05629b6c7a3c0b80
Umair Abbas <umairabbass786@gmail.com>
2022-11-25 00:27:47 +0500

    structuring project

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={{backgroundColor: 'green'}}>HEllo World</Text>
+    </View>
+  );
+};
+export default App;
