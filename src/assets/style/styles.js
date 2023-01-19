70f5986bc62ce503feaf6efb6b63c327894f6269
Umair Abbas <umairabbass786@gmail.com>
2023-01-19 05:40:16 +0500

    login screen created

--- src/assets/style/styles.js
+++ src/assets/style/styles.js
@@ -5,14 +5,55 @@ const {width, height} = Dimensions.get('screen');
 
 const styles = StyleSheet.create({
   mainContainer: {
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    // alignItems: 'center',
+    // justifyContent: 'space-between',
     flex: 1,
     backgroundColor: theme.colors.BG,
   },
   mainText: {
     color: theme.colors.BTN,
     fontSize: theme.colors.fontSize,
+  },
+  inputField: {
+    backgroundColor: 'white',
+    borderRadius: 15,
+    // width: '100%',
+    height: 45,
+    marginBottom: 10,
+    color: 'black',
+    paddingHorizontal: 10,
+  },
+  buttonDark: {
+    marginTop: 10,
+    backgroundColor: 'black',
+    height: 45,
+    borderRadius: 15,
+    alignItems: 'center',
+    padding: 10,
+  },
+  buttonLight: {
+    marginTop: 10,
+    backgroundColor: 'white',
+    height: 45,
+    borderRadius: 15,
+    alignItems: 'center',
+    padding: 10,
+  },
+  buttonTextDark: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 18,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  buttonTextLight: {
+    color: 'black',
+    textAlign: 'center',
+    fontSize: 18,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
 });
 
--- src/screens/Welcome/Welcome.js
+++ src/screens/Welcome/Welcome.js
@@ -1,16 +1,43 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, TextInput, TouchableOpacity, Alert } from 'react-native';
 import styles from '../../assets/style/styles';
 
 export default function Welcome() {
   return (
     <View style={styles.mainContainer}>
-      <View style={{ flex:0.40 }}>
+      <View style={{ flex:0.40, alignItems: 'center'}}>
         <Image
           source={require('../../assets/images/slr.png')}
           style={{ width: 200, height: 200, marginTop: 30 }}
           resizeMode={'contain'}
         />
+      </View>
+      <View style={{ flex: 0.60, marginHorizontal: 20 }}>
+        <Text style={{ color: 'white', marginBottom: 20, marginLeft: 5, textAlign: 'left', fontSize: 26 }}>Welcome {"\n"}
+          <Text style={{ fontSize: 16}}>Login to your Account</Text></Text>
+        <Text style={{ color: 'white', marginBottom: 5, marginLeft: 5}}>Email</Text>
+        <TextInput
+          style={styles.inputField}
+          placeholder="Enter your Email"
+          placeholderTextColor={'grey'}
+        />
+        <Text style={{ color: 'white', marginBottom: 5, marginLeft: 5 }}>Password</Text>
+        <TextInput
+          style={styles.inputField}
+          secureTextEntry={true}
+          placeholder="Enter your Password"
+          placeholderTextColor={'grey'}
+        />
+        <TouchableOpacity
+          style={styles.buttonDark}
+          onPress={() => Alert.alert('Login')}>
+          <Text style={styles.buttonTextDark}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonLight}
+          onPress={() => Alert.alert('Register')}>
+          <Text style={styles.buttonTextLight}>Create Account</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
